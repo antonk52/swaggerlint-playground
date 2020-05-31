7760d1caa7610005e23cf46fa4ba0fa3fa8b4a26
antonk52 <halloy52@gmail.com>
2020-05-31 15:17:07 +0300

    swaggerlint version in the header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import css from './style.module.css';
 
+import {version} from 'swaggerlint/package.json';
+
 export function Header({children}: {children: React.ReactNode}) {
     return (
         <header className={css.header}>
-            <h1>Swaggerlint Playground</h1>
+            <div className={css.star}>
+                <h1 className={css.name}>Swaggerlint</h1>
+                <span className={css.version}>v{version}</span>
+            </div>
             {children}
         </header>
     );
