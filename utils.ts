05bd7eaead59f7aca7aa1a3963210ba0c6ed0aab
antonk52 <halloy52@gmail.com>
2020-06-19 00:54:09 +0300

    remove yaml prettier parser

--- utils.ts
+++ utils.ts
@@ -1,13 +1,12 @@
 import prettier from 'prettier/standalone';
 import prettierBabelParser from 'prettier/parser-babel';
-import prettierYamlParser from 'prettier/parser-yaml';
 import SLdefaultConfig from 'swaggerlint/dist/defaultConfig';
 import {Config, Format} from 'types';
 
 export function prettify(arg: string, parser: Format): string {
     return prettier.format(arg, {
         parser,
-        plugins: [prettierBabelParser, prettierYamlParser],
+        plugins: [prettierBabelParser],
         tabWidth: 4,
     });
 }
