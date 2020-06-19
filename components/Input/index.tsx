9abd1cb799479480c62277f5757f913ece04b99b
antonk52 <halloy52@gmail.com>
2020-06-19 15:16:41 +0300

    custom properties for colors all the places

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -5,12 +5,14 @@ import css from './style.module.css';
 type Props = {
     value: string;
     onChange: (value: string) => void;
+    placeholder?: string;
 };
 
-export const Input = ({value, onChange}: Props) => (
+export const Input = ({value, onChange, placeholder}: Props) => (
     <input
         type="text"
         value={value}
+        placeholder={placeholder}
         onChange={(e) => onChange(e.target.value)}
         className={css.input}
     />
